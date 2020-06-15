fa631fbe193242d54487997a5aee57e5a8503815
Stephanie Butenhof <1divinedevotion@gmail.com>
2020-06-14 22:20:18 -0500

    React-router-dom, PrivateRoute

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+	return (
+		<Route
+			{...rest}
+			render={(props) => {
+				if (localStorage.getItem('token')) {
+					return <Component />;
+				} else {
+					console.log('Redirect to login');
+					return <Redirect to='/login' />;
+				}
+			}}
+		/>
+	);
+};
+
+export default PrivateRoute;
